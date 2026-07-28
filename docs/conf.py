b58782478057cd06653c0e724b0b0b5161f3aa76
taxguide <singhparash06@gmail.com>
2026-07-28 17:57:51 +0530

    Create conf.py

---
+++ docs/conf.py
@@ -0,0 +1,21 @@
+# Configuration file for the Sphinx documentation builder.
+
+project = 'Webroot.com/Safe'
+author = 'Webroot.com/Safe'
+release = '1.0'
+
+extensions = []
+
+# Templates
+templates_path = ['_templates']
+
+exclude_patterns = []
+
+html_theme = 'alabaster'
+
+# Static files
+html_static_path = ['_static']
+
+language = 'en'
+
+html_title = "Webroot.com/Safe"
